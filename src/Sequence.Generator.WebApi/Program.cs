08528eef670f6b691ce2967e98f0b44065eaca0e
p1971 <pete@madhippy.com>
2022-04-21 22:54:14 +0100

    Fixing up configs for running under docker

--- src/Sequence.Generator.WebApi/Program.cs
+++ src/Sequence.Generator.WebApi/Program.cs
@@ -6,10 +6,13 @@ WebApplication app = builder.Build();
 
 string redisConnectionString = Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING") ?? "localhost:6379";
 
+System.Console.WriteLine($"Using redis connection string: {redisConnectionString}");
+
 ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(redisConnectionString);
 
 app.MapGet(@"/api/seq/{key}/{prefix=}/{suffix=}/{digits:int=10}", async (string key, string prefix, string suffix, int digits) =>
 {
+    System.Console.WriteLine($"... : {key}");
     try
     {
         IDatabase db = redis.GetDatabase();
@@ -22,6 +25,8 @@ app.MapGet(@"/api/seq/{key}/{prefix=}/{suffix=}/{digits:int=10}", async (string 
     }
     catch (Exception ex)
     {
+        System.Console.WriteLine(ex);
+
         return Results.BadRequest(ex.Message);
     }
 });
